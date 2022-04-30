9265e33d18a36e60e7670e8b5053d1b0295fc997
Sasikant Biswal <biswalsashikant@gmail.com>
2022-05-01 01:58:56 +0530

    Added some logical code

--- fibbonacci-memoization/index.js
+++ fibbonacci-memoization/index.js
@@ -1,10 +1,10 @@
 // Plain Fibbonacci version
 function fib (n) {
-    if (n <=0) {
+    if (n === 0) {
         return 0;
     }
 
-    if (n<=1) {
+    if (n === 1) {
         return 1;
     }
 
@@ -19,9 +19,12 @@ function fibMemo(n, memo = {0:0, 1:1}) {
 
     const result = fibMemo(n-1, memo) + fibMemo(n-2, memo);
     memo[n] = result;
+    // console.log(result)
     return result;
 }
 
 console.log(fib(5));
 // console.log(fib(50));  // error
 console.log(fibMemo(50));
+
+// fibMemo()
