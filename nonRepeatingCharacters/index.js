3a5e9680788540572845184c047008edba29fa21
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-10-11 18:11:03 +0530

    Added some logical challenges

--- nonRepeatingCharacters/index.js
+++ nonRepeatingCharacters/index.js
@@ -1,8 +1,9 @@
-let str = "abcab";
+let str = "abcabcd";
 
 function nonRepeatingCharacter (str) {
     for (let i = 0; i < str.length; i++) {
         let j = str.charAt(i)
+        console.log(j)
 
         if (str.indexOf(j) === str.lastIndexOf(j)) {
             return j;
